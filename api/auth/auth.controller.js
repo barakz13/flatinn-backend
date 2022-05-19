f243d9f129a5034c4c5e15f118837d2bb146efaa
Barak Levav <levavbarak@gmail.com>
2022-05-19 15:56:44 +0300

    test

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,6 +1,5 @@
 const authService = require('./auth.service');
 const logger = require('../../services/logger.service');
-///// בדיקה
 async function login(req, res) {
   const { username, password } = req.body;
   try {
