577f3e6844a582c3b3d8b4dc635e81cc51a07802
Barak Levav <levavbarak@gmail.com>
2022-05-19 20:52:03 +0300

    trying to fix stays not loading in heroku

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,3 @@
 module.exports = {
-  // dbURL: process.env.MONGO_URL,
   dbURL: process.env.HEROKU_MONGO_URL,
-  // dbURL:
-  // 'mongodb+srv://flatinn:flatinn123456@cluster0.ps19i.mongodb.net/flatinn_db?retryWrites=true&w=majority',
-  // dbURL:
-  // 'mongodb+srv://barakz13:barakl6435@baraks-cluster.hudrh.mongodb.net/flatinn_db?retryWrites=true&w=majority',
 };
