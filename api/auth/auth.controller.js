e9da9c8b3fd1fdf54775c808d3cf639581936eea
Barak Levav <levavbarak@gmail.com>
2022-05-19 15:53:30 +0300

    test gitignore

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,43 +1,45 @@
-const authService = require('./auth.service')
-const logger = require('../../services/logger.service')
-
+const authService = require('./auth.service');
+const logger = require('../../services/logger.service');
+///// בדיקה
 async function login(req, res) {
-  const { username, password } = req.body
+  const { username, password } = req.body;
   try {
-    const user = await authService.login(username, password)
-    req.session.user = user
-    res.json(user)
+    const user = await authService.login(username, password);
+    req.session.user = user;
+    res.json(user);
   } catch (err) {
-    logger.error('Failed to Login ' + err)
-    res.status(401).send({ err: 'Failed to Login' })
+    logger.error('Failed to Login ' + err);
+    res.status(401).send({ err: 'Failed to Login' });
   }
 }
 
 async function signup(req, res) {
   try {
-    const { username, password, fullname } = req.body
+    const { username, password, fullname } = req.body;
     // Never log passwords
     // logger.debug(fullname + ', ' + username + ', ' + password)
-    const account = await authService.signup(username, password, fullname)
-    logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
-    const user = await authService.login(username, password)
-    req.session.user = user
-    res.json(user)
+    const account = await authService.signup(username, password, fullname);
+    logger.debug(
+      `auth.route - new account created: ` + JSON.stringify(account)
+    );
+    const user = await authService.login(username, password);
+    req.session.user = user;
+    res.json(user);
   } catch (err) {
-    logger.error('Failed to signup ' + err)
-    res.status(500).send({ err: 'Failed to signup' })
+    logger.error('Failed to signup ' + err);
+    res.status(500).send({ err: 'Failed to signup' });
   }
 }
 
 async function logout(req, res) {
   try {
     // req.session.user = null
-    let user123 = req.session.user
-    console.log("user trying to LOGOUT=",user123)
-    req.session.destroy()
-    res.send({ msg: 'Logged out successfully' })
+    let user123 = req.session.user;
+    console.log('user trying to LOGOUT=', user123);
+    req.session.destroy();
+    res.send({ msg: 'Logged out successfully' });
   } catch (err) {
-    res.status(500).send({ err: 'Failed to logout' })
+    res.status(500).send({ err: 'Failed to logout' });
   }
 }
 
@@ -45,4 +47,4 @@ module.exports = {
   login,
   signup,
   logout,
-}
+};
