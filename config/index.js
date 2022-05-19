00212b268b5b2c4c676246da761422fda7ece518
Barak Levav <levavbarak@gmail.com>
2022-05-19 20:46:05 +0300

    trying to fix stays not loading in heroku

--- config/index.js
+++ config/index.js
@@ -1,13 +1,14 @@
 var config;
 
 // keys.js - figure out what set of credentials to return
-if (false && process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
   config = require('./prod');
+  console.log('prod');
 } else {
   // we are in development - return the dev keys!!!
   config = require('./dev');
-
+  console.log('dev');
   //
 }
 
