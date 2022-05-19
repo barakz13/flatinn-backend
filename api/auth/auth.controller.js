2243fba2f8e876863ff8421f47dfc45727e0df33
Barak Levav <levavbarak@gmail.com>
2022-05-19 16:02:12 +0300

    test

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,6 +1,7 @@
 const authService = require('./auth.service');
 const logger = require('../../services/logger.service');
 async function login(req, res) {
+  /////// test
   const { username, password } = req.body;
   try {
     const user = await authService.login(username, password);
